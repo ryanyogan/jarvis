d57213194805583a01bfecdc4b2943770293e51e
Ryan Yogan <ryanyogan@gmail.com>
2023-08-04 17:32:25 -0500

    redirect by session until middleware works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Chat from "@/components/chat/chat";
 import { nanoid } from "ai";
+import { redirect } from "next/navigation";
+import { auth } from "../../auth";
 
 export const runtime = "edge";
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  const session = await auth();
+  if (!session?.user) {
+    return redirect("/sign-in");
+  }
+
   const id = nanoid();
 
   return <Chat id={id} />;
